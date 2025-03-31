44ead2eb2a9e3aea9e17835c8197d39043d78fa6
Gabriele <gabriele.rosa@galileo.galileicrema.it>
2025-03-31 09:25:30 +0200

    prova client

---
+++ prova_client.py
@@ -0,0 +1,25 @@
+import bluetooth
+import pickle
+
+server_address = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
+
+def send_transaction():
+    client_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+
+    try:
+        client_sock.connect((server_address, 1))
+        print(f"[CLIENT] Connesso al server {server_address}")
+
+        transaction = ("Deposito", "2024-03-31", 150.75, "Versamento stipendio")
+        data = pickle.dumps(transaction)
+
+        client_sock.send(data)
+        print(f"[CLIENT] Transazione inviata: {transaction}")
+
+    except Exception as e:
+        print(f"[CLIENT] Errore: {e}")
+
+    finally:
+        client_sock.close()
+        print("[CLIENT] Connessione chiusa.")
+
