5e819f013ac7533be8825a48d11d1c8e90872a94
Gabriele <gabriele.rosa@galileo.galileicrema.it>
2025-04-14 09:16:00 +0200

    prova bluetooth

--- bt_client.py
+++ bt_client.py
@@ -1,15 +1,36 @@
 import bluetooth
+import uuid
 
-server_address = "D8:3A:DD:ED:86:AE"  # Inserisci l'indirizzo MAC del Raspberry server
+service_uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"  # Deve essere lo stesso del server
+
+# Scansiona i dispositivi Bluetooth per trovare il server
+nearby_devices = bluetooth.discover_devices(duration=8, lookup_names=True)
+
+server_address = None
+
+print("Dispositivi Bluetooth trovati:")
+for addr, name in nearby_devices:
+    print(f"{addr} - {name}")
+    # Supponiamo che il server sia chiamato "RaspberryPiServer", cambialo in base alle tue esigenze
+    if "RaspberryPiServer" in name:
+        server_address = addr
+        break
+
+if server_address is None:
+    print("Server non trovato. Assicurati che sia visibile.")
+    exit()
+
+print(f"Connessione al server con indirizzo: {server_address}")
 
 client_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
 client_sock.connect((server_address, 1))
 
-print("Connessione avviata al server!")
+print("Connessione stabilita!")
 
-client_sock.send("Ciao Raspberry!".encode())
+client_sock.send("Ciao! Questo messaggio usa UUID.".encode())
 
 client_sock.close()
+
 
 
 
--- bt_server.py
+++ bt_server.py
@@ -1,10 +1,20 @@
 import bluetooth
+import uuid
+
+# Genera un UUID specifico per il servizio
+service_uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
 
 server_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
 server_sock.bind(("", bluetooth.PORT_ANY))
 server_sock.listen(1)
 
-print("In attesa di connessioni Bluetooth...")
+bluetooth.advertise_service(server_sock, "RaspberryPiService",
+                            service_id=service_uuid,
+                            service_classes=[service_uuid, bluetooth.SERIAL_PORT_CLASS],
+                            profiles=[bluetooth.SERIAL_PORT_PROFILE])
+
+print(f"Server in attesa di connessioni... (UUID: {service_uuid})")
+
 client_sock, client_info = server_sock.accept()
 print(f"Connessione accettata da {client_info}")
 
