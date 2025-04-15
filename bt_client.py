efa0e153f2b1d1ad584129b05e8044c39f43c1cc
Gabriele <gabriele.rosa@galileo.galileicrema.it>
2025-04-15 18:49:11 +0200

    prova bluetooth

--- bt_client.py
+++ bt_client.py
@@ -1,39 +1,36 @@
 import bluetooth
+import uuid
 
-# UUID del servizio a cui ci vogliamo connettere
+# MAC address del dispositivo server (sostituisci con quello corretto)
+target_address = "B8:27:EB:62:DD:D0"  # Es: "00:1A:7D:DA:71:13"
+
+# UUID del servizio da cercare
 service_uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
 
-print("Ricerca servizi Bluetooth...")
+# Trova il servizio Bluetooth sul dispositivo target (server)
+services = bluetooth.find_service(uuid=service_uuid, address=target_address)
 
-# Trova i servizi che espongono questo UUID
-services = bluetooth.find_service(uuid=service_uuid)
-
-if len(services) == 0:
-    print("Nessun servizio trovato.")
+if not services:
+    print("Servizio non trovato!")
     exit()
 
+# Se il servizio è trovato, ottieni la porta e l'indirizzo del servizio
 first_match = services[0]
-host = first_match["host"]
 port = first_match["port"]
+host = first_match["host"]
 
-print(f"Trovato servizio su {host}, porta {port}")
+print(f"Servizio trovato su {host}:{port}")
 
+# Connessione al server utilizzando l'indirizzo MAC e la porta
 sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
 sock.connect((host, port))
-print("Connesso al server!")
 
-try:
-    # Ricevi messaggio dal server
-    data = sock.recv(1024)
-    print("Messaggio ricevuto:", data.decode())
+# Ricevi i dati dal server
+data = sock.recv(1024)
+print(f"Ricevuto dal server: {data.decode()}")
 
-    # Rispondi
-    response = "Ciao server, ricevuto forte e chiaro!"
-    sock.send(response)
-    print("Risposta inviata al server.")
+# Invia una risposta al server
+sock.send("Ciao dal client!")
 
-except OSError as e:
-    print("Errore:", e)
-
-print("Chiusura connessione")
+# Chiudi la connessione
 sock.close()
--- bt_server.py
+++ bt_server.py
@@ -1,42 +1,39 @@
 import bluetooth
 import uuid
 
+# Creazione del socket Bluetooth per il server
 server_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+
+# Lega il socket a una porta specifica
 server_sock.bind(("", bluetooth.PORT_ANY))
 server_sock.listen(1)
 
-port = server_sock.getsockname()[1]
-
-# UUID del servizio che vogliamo pubblicare
+# UUID per il servizio Bluetooth
 service_uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
 
+# Pubblica il servizio Bluetooth in ascolto per connessioni
 bluetooth.advertise_service(
     server_sock,
-    "ChatService",
+    "TestServer",
     service_id=service_uuid,
     service_classes=[service_uuid, bluetooth.SERIAL_PORT_CLASS],
     profiles=[bluetooth.SERIAL_PORT_PROFILE],
 )
 
-print(f"In attesa di connessione su UUID {service_uuid}, porta {port}...")
+print("Server in attesa di connessione...")
 
-client_sock, client_info = server_sock.accept()
-print(f"Connessione accettata da {client_info}")
+# Attende una connessione da un client
+client_sock, address = server_sock.accept()
+print(f"Connessione stabilita con {address}")
 
-try:
-    # Invia messaggio iniziale
-    msg = "Benvenuto, client!"
-    client_sock.send(msg)
-    print("Messaggio inviato:", msg)
+# Invio di un messaggio al client
+client_sock.send("Ciao dal server!\n")
 
-    # Ricevi risposta
-    data = client_sock.recv(1024)
-    print("Risposta del client:", data.decode())
+# Riceve il messaggio dal client
+data = client_sock.recv(1024)
+print(f"Ricevuto dal client: {data.decode()}")
 
-except OSError as e:
-    print("Errore:", e)
-
-print("Chiusura connessione")
+# Chiude la connessione
 client_sock.close()
 server_sock.close()
 
