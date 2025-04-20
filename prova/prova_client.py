72a32655c296429534ae835e2d0975d67137aeb3
Gabriele <gabriele.rosa@galileo.galileicrema.it>
2025-04-20 15:55:32 +0200

    prova

--- prova/prova_client.py
+++ prova/prova_client.py
@@ -5,27 +5,27 @@ server_address = "B8:27:EB:62:DD:D0"
 
 def send_transaction():
     client_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+    while(True):
+        try:
+            client_sock.connect((server_address, 1))
+            print(f"[CLIENT] Connesso al server {server_address}")
 
-    try:
-        client_sock.connect((server_address, 1))
-        print(f"[CLIENT] Connesso al server {server_address}")
+            transaction = "entrata 2024-03-31 150.75 Versamento stipendio"
+            serialized = struct.pack('!50s', transaction.encode('utf-8'))  # Esempio di come usare struct
 
-        transaction = "entrata 2024-03-31 150.75 Versamento stipendio"
-        serialized = struct.pack('!50s', transaction.encode('utf-8'))  # Esempio di come usare struct
+            client_sock.send(serialized)
+            print(f"[CLIENT] Transazione inviata: {transaction}")
 
-        client_sock.send(serialized)
-        print(f"[CLIENT] Transazione inviata: {transaction}")
+            data = client_sock.recv(1024)
+            saldo = struct.unpack('!d', data)  # Esempio di come usare struct per deserializzare un saldo
+            print(f"[CLIENT] Saldo aggiornato ricevuto: {saldo[0]} €")
 
-        data = client_sock.recv(1024)
-        saldo = struct.unpack('!d', data)  # Esempio di come usare struct per deserializzare un saldo
-        print(f"[CLIENT] Saldo aggiornato ricevuto: {saldo[0]} €")
+        except Exception as e:
+            print(f"[CLIENT] Errore: {e}")
 
-    except Exception as e:
-        print(f"[CLIENT] Errore: {e}")
-
-    finally:
-        #client_sock.close()
-        print("[CLIENT] Connessione chiusa")
+        finally:
+            #client_sock.close()
+            print("[CLIENT] Connessione chiusa")
 
 if __name__ == "__main__":
     send_transaction()
